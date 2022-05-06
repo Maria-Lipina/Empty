23a71baf76517c231e755e97395a9772d6209bac
Maria Lipina <m.o.lip714@gmail.com>
2022-05-06 13:29:42 +0300

    task 1 added

---
+++ 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+using static System.Convert;
+
+WriteLine("1. По двум заданным числам проверять является ли первое квадратом второго");
+
+bool IsSquare(int square, int number)
+{
+if (Math.Sqrt(square) == number) return true;
+else return false;
+}
+
+WriteLine(IsSquare(ToInt32(ReadLine()),ToInt32(ReadLine())));
