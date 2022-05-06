9d76a83d3a0840a01e0b484de76337af9da5bdc1
Maria Lipina <m.o.lip714@gmail.com>
2022-05-06 13:35:36 +0300

    task 2 added

---
+++ 2/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+using static System.Convert;
+
+WriteLine("2. Даны два числа. Показать большее и меньшее число");
+
+void Compare (int x, int y)
+{
+if (y>x) Console.WriteLine(y + " больше " + x);
+else Console.WriteLine(x +" больше "+ y);
+}
+
+int x3 = ToInt32(ReadLine());
+int x4 = ToInt32(ReadLine());
+Compare(x3, x4);
