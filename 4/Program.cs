8de00941366620910f8b703328d223048d5f47e1
Maria Lipina <m.o.lip714@gmail.com>
2022-05-07 22:36:41 +0300

    tasks 3, 4 added

---
+++ 4/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+using static System.Convert;
+
+WriteLine("4. Найти максимальное из 3 чисел");
+
+int num = ToInt32(ReadLine());
+int num1 = ToInt32(ReadLine());
+int num2 = ToInt32(ReadLine());
+
+int max = num;
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+WriteLine(max);
