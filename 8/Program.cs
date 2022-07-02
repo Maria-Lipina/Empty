e39f9f846175484b5bb32ecdbb93f077562c497b
Maria Lipina <m.o.lip714@gmail.com>
2022-07-02 23:12:49 +0300

    добавлена задача 8

---
+++ 8/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using static System.Convert;
+
+WriteLine("8. Показать четные числа от 1 до N");
+
+int[] SelectEvens(int n)
+{
+int [] result = new int [n];
+int len = 0;
+
+for (int i = 0; i<= result.Length; i++)
+{
+    if (i%2==0)
+    {
+    result [len] = i;
+    len++;
+    }
+}
+Array.Resize(ref result, len);
+return result;
+}
+
+void PrintArray(int[] collect)
+{
+    for (int pos = 0; pos < collect.Length; pos ++) Write($"{collect[pos]} ");
+    WriteLine();
+    WriteLine();
+}
+
+PrintArray(SelectEvens(ToInt32(ReadLine())));
