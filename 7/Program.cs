39c7062eb2b83ca828ae66d6bfd2f864b326a793
Maria Lipina <m.o.lip714@gmail.com>
2022-07-02 23:07:49 +0300

    добавлена задача 7

---
+++ 7/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+using static System.Convert;
+
+void PrintArray(int[] collect)
+{
+    for (int pos = 0; pos < collect.Length; pos ++) Write($"{collect[pos]} ");
+    WriteLine();
+    WriteLine();
+}
+
+int [] FillArray(int N)
+{
+int[] collect = new int[N+N+1];
+for (int pos = 0; pos < collect.Length; pos++) 
+{
+    collect[pos] = -N;
+N--; // тот самый момент, когда сначала написал код и только потом понял, почему он работает как надо.
+}
+return collect;
+}
+PrintArray(FillArray(ToInt32(ReadLine())));
