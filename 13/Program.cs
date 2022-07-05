d9bb1139731683387fe059692c9bb85b1a5b3fb9
Maria Lipina <m.o.lip714@gmail.com>
2022-07-05 21:09:26 +0300

    добавлена задача 13

---
+++ 13/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+using static System.Convert;
+
+WriteLine();
+WriteLine("13. Выяснить, кратно ли число заданному, если нет, вывести остаток.");
+
+bool FactorCheck (int number, int factor)
+{
+    if (number % factor == 0) return true;
+    else return false;
+}
+
+int number3 = Convert.ToInt32(ReadLine());
+Write("Проверить на кратность: ");
+int factor = Convert.ToInt32(ReadLine());
+
+if(FactorCheck(number3, factor)) WriteLine("Кратно");
+else {Write("Не кратно. Остаток: " + number3 % factor);}
