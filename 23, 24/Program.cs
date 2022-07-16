edbeb7200f1712e5551a060fac88180596fc3769
Maria Lipina <m.o.lip714@gmail.com>
2022-07-16 22:38:27 +0300

    задачи 23 и 24

---
+++ 23, 24/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int [] FillArray1(int A)
+{
+int[] collect = new int [A];
+for (int pos = 0; pos < collect.Length; pos++) collect[pos] = pos+1;
+return collect;
+}
+
+void PrintPowers(int [] N, int power)
+{
+for (int i = 0; i < N.Length; i++)
+{
+   WriteLine($"{N[i]}^{power} = {Math.Pow(N[i], power)}");
+} 
+}
+
+PrintPowers(FillArray1(ToInt32(ReadLine())),2);
+
+
+WriteLine();
+WriteLine("24. Найти кубы чисел от 1 до N");
+
+PrintPowers(FillArray1(ToInt32(ReadLine())),3);
