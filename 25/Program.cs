79b94eb97b561c4b29465eb6638b20ac9416a037
Maria Lipina <m.o.lip714@gmail.com>
2022-07-16 22:38:43 +0300

    задача 25

---
+++ 25/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int [] FillArray1(int A)
+{
+int[] collect = new int [A];
+for (int pos = 0; pos < collect.Length; pos++) collect[pos] = pos+1;
+return collect;
+}
+
+WriteLine();
+WriteLine("25. Найти сумму чисел от 1 до А");
+
+int ArraySum (int[] A)
+{int sum = 1;
+for (int i = 1; i < A.Length; i++)
+{
+    sum = sum + A[i];
+}
+return sum;
+}
+
+WriteLine(ArraySum(FillArray1(ToInt32(ReadLine()))));
+
+
