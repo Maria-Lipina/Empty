61475a71ed5879f592039203ded373a0073928c2
Maria Lipina <m.o.lip714@gmail.com>
2022-07-16 22:38:58 +0300

    задача 26

---
+++ 26/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using static System.Convert;
+
+WriteLine();
+WriteLine("26. Возведите число А в натуральную степень B используя цикл");
+
+int Power (int A, int B)
+{int result = A;
+for (int i = 1; i < B+1; i++)
+{
+    result = result * A;
+}
+return result;
+}
+
+WriteLine("A = ");
+int A1 = ToInt32(ReadLine());
+WriteLine("B = ");
+int B = ToInt32(ReadLine());
+WriteLine(Power(A1,B));
