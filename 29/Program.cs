20f347378aad21b9372b84840d535feaf837d6b8
Maria Lipina <m.o.lip714@gmail.com>
2022-07-19 22:40:03 +0300

    задача 29

---
+++ 29/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int [] FillArray1(int A)
+{
+int[] collect = new int [A];
+for (int pos = 0; pos < collect.Length; pos++) collect[pos] = pos+1;
+return collect;
+}
+
+WriteLine("29. Написать программу вычисления произведения чисел от 1 до N");
+
+long ArrayProduct (int[] A)
+{long prod = A[0];
+for (int i = 1; i < A.Length; i++)
+{
+prod = prod * A[i];
+}
+return prod;
+}
+
+WriteLine(ArrayProduct(FillArray1(ToInt32(ReadLine()))));
