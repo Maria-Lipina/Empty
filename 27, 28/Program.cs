44097550f77caf073940220c9731266202305a10
Maria Lipina <m.o.lip714@gmail.com>
2022-07-19 22:34:32 +0300

    задачи 27, 28 (от оригинала отличается тем, что для задачи 27 добавлен digs.Length

---
+++ 27, 28/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int [] Digits(int n)
+{
+    int [] result = new int [Convert.ToString(n).Length];
+    if (n == 0) result [0] = n;
+    else
+    {
+        for (int i = 0; i < result.Length; i++)
+        {
+            int o = n % 10;
+            result[i] = o; // не совсем очевидно, но можно догадаться, что полностью строка будет result = result + o
+            n /= 10;
+        }
+    }
+    return result;
+}
+
+int ArraySum (int[] A)
+{int sum = 1;
+for (int i = 1; i < A.Length; i++)
+{
+    sum = sum + A[i];
+}
+return sum;
+}
+
+int [] digs = Digits(ToInt32(ReadLine()));
+
+WriteLine("27. Определить количество цифр в числе");
+
+WriteLine(digs.Length);
+
+WriteLine();
+WriteLine("28. Подсчитать сумму цифр в числе");
+
+WriteLine(ArraySum(digs));
