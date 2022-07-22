e4403680078408fa41e0ecad83e2c3c7af7cfdcc
Maria Lipina <m.o.lip714@gmail.com>
2022-07-22 22:47:57 +0300

    Добавлены задачи 31 и 32

---
+++ 31, 32/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int[] CreateMyArray(int length)
+{
+    return new int[length];
+}
+
+void FillMyArray(int[] collection, int minValue, int maxValue) // А тут войд можно и нужно, потому что метод только заполняет
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(minValue, maxValue + 1);
+    }
+}
+
+string PrintMyArray(int[] collect)
+{
+    string outputString = String.Empty;
+    for (int pos = 0; pos < collect.Length; pos++) outputString += $"{collect[pos]} ";
+    return outputString;
+}
+
+
+WriteLine();
+WriteLine("31. Задать массив из 8 элементов и вывести их на экран");
+WriteLine("32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран ");
+
+
+int[] arr1 = CreateMyArray(8);
+FillMyArray(arr1, 0, 1);
+WriteLine(PrintMyArray(arr1));
