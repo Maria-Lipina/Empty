7f7758583d15d06cd27c55d81d5670bf5ea678da
Maria Lipina <m.o.lip714@gmail.com>
2022-09-06 19:47:43 +0300

    Добавлена задача 45 + уьрана пометка "не сделано" в Readme

---
+++ 45/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+using static System.Convert;
+
+WriteLine("45. Показать числа Фибоначчи");
+
+double Fibonacci (int n)
+{
+    if(n == 1 || n == 0) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 0; i < 40; i++)
+{
+    WriteLine(Fibonacci(i));
+}
