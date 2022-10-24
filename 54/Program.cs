6f23ca3f2e73d6c279381c87a850ff82d47264aa
Maria Lipina <m.o.lip714@gmail.com>
2022-10-24 10:58:46 +0300

    задача 54 добавлена

---
+++ 54/Program.cs
@@ -0,0 +1,46 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int[,] Create2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+
+void Fill2DArray(int[,] collection, int minValue, int maxValue)
+{
+    for (int i = 0; i < collection.GetLength(0); i++)
+    {
+        for (int j = 0; j < collection.GetLength(1); j++)
+        {
+            collection[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+
+    }
+}
+
+string Print2DArray(int[,] collection)
+{
+    string outputString = String.Empty;
+    for (int i = 0; i < collection.GetLength(0); i++)
+    {
+        for (int j = 0; j < collection.GetLength(1); j++) // И помни, что длина строки определяется количеством столбцов. И наоборот длина столбца определяется количеством строк.
+        {
+            outputString += $"{collection[i, j]} | ";
+        }
+        outputString += "\n";
+    }
+    return outputString;
+}
+
+WriteLine("54. В матрице чисел найти сумму элементов главной диагонали");
+
+int [,] cd = Create2DArray (20, 20);
+Fill2DArray(cd, 10, 100);
+WriteLine(Print2DArray(cd));
+
+int sum = 0;
+for (int i = 0; i < cd.GetLength(1); i++)
+{
+sum = sum + cd [i, i];   
+}
+WriteLine(sum);
