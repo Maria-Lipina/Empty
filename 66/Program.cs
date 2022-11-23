b7411cc1051df06e3d363856202b00bcc8895e18
Maria Lipina <m.o.lip714@gmail.com>
2022-11-23 18:52:36 +0300

    добавлена задача 66

---
+++ 66/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+using static System.Convert;
+
+WriteLine();
+WriteLine("66. Показать натуральные числа от 1 до N, N задано");
+
+string From1toN (int n)
+{
+if (n != 0)
+return From1toN(n-1) + " " + n;
+else return string.Empty;
+}
+
+WriteLine(From1toN(10));
