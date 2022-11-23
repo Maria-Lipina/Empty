02576f6ba4a3a3045e9bcdae4c007948864d9c7e
Maria Lipina <m.o.lip714@gmail.com>
2022-11-23 18:54:38 +0300

    Добавлена задача 67

---
+++ 67/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+using static System.Convert;
+
+WriteLine();
+WriteLine("67. Показать натуральные числа от N до 1, N задано");
+
+string FromNto1 (int n)
+{
+if (n != 0)
+return n + " " + FromNto1(n-1);
+else return string.Empty;
+}
+
+WriteLine(FromNto1(10));
