b0220ce016c1b43b7ea36c5be19295aaa1cdb799
Maria Lipina <m.o.lip714@gmail.com>
2023-02-27 10:38:26 +0300

    Задача 71 добавлена

---
+++ 71/Program.cs
@@ -0,0 +1,13 @@
+﻿WriteLine();
+WriteLine("71. Написать программу вычисления функции Аккермана");
+
+double Ackerman (double m, double n)
+{
+    if (m == 0) return n + 1;
+    if ((m > 0) && (n == 0)) return Ackerman (m-1, 1);
+    return Ackerman (m-1, Ackerman(m, n-1));
+}
+WriteLine(Ackerman(0, 0));
+WriteLine(Ackerman(2, 1));
+WriteLine(Ackerman(2, 3));
+WriteLine(Ackerman(3, 5));
