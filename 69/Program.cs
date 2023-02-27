1da41732d86901b3e49959521dccdd48d53e5694
Maria Lipina <m.o.lip714@gmail.com>
2023-02-27 10:33:43 +0300

    Задача 69 добавлена

---
+++ 69/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+using static System.Convert;
+
+WriteLine();
+WriteLine("69. Найти сумму элементов от M до N, N и M заданы");
+
+int SumFromMtoN(int m, int n)
+{
+    if (n < m)
+    {     
+       return m + SumFromMtoN(m - 1, n);
+    }
+    if (m < n)
+    {
+        return n + SumFromMtoN(m, n-1); 
+    }
+    return n;
+}
+
+WriteLine(SumFromMtoN(10, 30)); 
+WriteLine(SumFromMtoN(30, 10));
