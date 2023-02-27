f7bc4a5e4b7989b7a9420e0bc5cc4762da46d9f1
Maria Lipina <m.o.lip714@gmail.com>
2023-02-27 10:35:16 +0300

    Задача 70 добавлена

---
+++ 70/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+using static System.Convert;
+
+
+WriteLine();
+WriteLine("70. Найти сумму цифр числа");
+
+int SumDig (int n)
+{
+    int dig = n % 10;
+    n = n / 10;
+    if (n != 0) return dig + SumDig(n);
+    else return 1;
+}
+
+WriteLine(SumDig(123456789));
