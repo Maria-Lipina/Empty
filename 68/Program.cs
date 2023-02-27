6fc6bd8fe83ca881264d24d047205a486400f209
Maria Lipina <m.o.lip714@gmail.com>
2023-02-27 10:28:25 +0300

    Задача 68 добавлена

---
+++ 68/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+using static System.Convert;
+
+WriteLine();
+WriteLine("68. Показать натуральные числа от M до N, N и M заданы");
+
+string FromMtoN (int m, int n)
+{
+if (n > m) return m + " " + FromMtoN(m + 1, n);
+if (n < m) return m + " " + FromMtoN(m - 1, n);
+return n.ToString();
+}
+
+WriteLine(FromMtoN(10, 30));
+WriteLine(FromMtoN(30, 10));
